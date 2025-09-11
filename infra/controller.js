85bdf05ce5945882d5264152e5c66291f9bf0944
RafaDSan <rafaeldss12@gmail.com>
2025-09-11 19:09:25 -0300

    fix export in controller.js

--- infra/controller.js
+++ infra/controller.js
@@ -1,27 +1,34 @@
 import {
-    InternalServerError,
-    MethodNotAllowedError,
-    ValidationError,
-    NotFoundError,
-  } from "infra/errors";
-  
-  function onNoMAtchHandler(request, response) {
-    const publicErrorObject = new MethodNotAllowedError();
-    response.status(publicErrorObject.statusCode).json(publicErrorObject);
+  InternalServerError,
+  MethodNotAllowedError,
+  ValidationError,
+  NotFoundError,
+} from "infra/errors";
+
+function onNoMatchHandler(request, response) {
+  const publicErrorObject = new MethodNotAllowedError();
+  response.status(publicErrorObject.statusCode).json(publicErrorObject);
+}
+
+function onErrorHandler(error, request, response) {
+  if (error instanceof ValidationError || error instanceof NotFoundError) {
+    return response.status(error.statusCode).json(error);
   }
-  
-  function onErrorHandler(error, request, response) {
-    if (error instanceof ValidationError || error instanceof NotFoundError) {
-      return response.status(error.statusCode).json(error);
-    }
-  
-    const publicErrorObject = new InternalServerError({
-      cause: error,
-    });
-  
-    console.error(publicErrorObject);
-  
-    response.status(publicErrorObject.statusCode).json(publicErrorObject);
-  }
-  
+
+  const publicErrorObject = new InternalServerError({
+    cause: error,
+  });
+
+  console.error(publicErrorObject);
+
+  response.status(publicErrorObject.statusCode).json(publicErrorObject);
+}
+
+const controller = {
+  errorHandlers: {
+    onNoMatch: onNoMatchHandler,
+    onError: onErrorHandler,
+  },
+};
+
 export default controller;
