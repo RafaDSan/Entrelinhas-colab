a1c5f677b9b6bf8374c7468dd8cd21231fd8915e
RafaDSan <rafaeldss12@gmail.com>
2025-09-19 22:16:42 -0300

    adds GET tests for search_books api

--- tests/api/v1/search_books/get.test.js
+++ tests/api/v1/search_books/get.test.js
@@ -1,12 +1,20 @@
-test("GET to /api/v1/search_books should return 200", async () => {
+test("GET to /api/v1/search-books should return 200", async () => {
   const response = await fetch(
-    "http://localhost:3000/api/v1/search_books?q=dune"
+    "http://localhost:3000/api/v1/search-books?q=dune"
   );
   expect(response.status).toBe(200);
 
   const responseBody = await response.json();
+  console.log(responseBody);
 
   expect(Array.isArray(responseBody)).toBe(true);
 
   expect(responseBody.length).toBeGreaterThan(0);
+
+  expect(responseBody[0]).toHaveProperty("key");
+  expect(responseBody[0]).toHaveProperty("title");
+  expect(responseBody[0]).toHaveProperty("author_name");
+  expect(responseBody[0]).toHaveProperty("cover_id");
+  expect(responseBody[0]).toHaveProperty("cover_url");
+  expect(responseBody[0]).toHaveProperty("first_publish_year");
 });
