e974b7c7a90150c91393ecb85f64364e92fabc72
RafaDSan <rafaeldss12@gmail.com>
2025-10-06 10:44:57 -0300

    refactor `books.js`

--- models/book.js
+++ models/book.js
@@ -1,15 +1,12 @@
 import database from "infra/database";
 
 async function create(bookDataFromApi) {
-  const dbClient = await database.getNewClient();
+  const authorName = bookDataFromApi.author_name
+    ? bookDataFromApi.author_name[0]
+    : null;
 
-  try {
-    const authorName = bookDataFromApi.autor_name
-      ? bookDataFromApi.author_name[0]
-      : null;
-
-    const result = await dbClient.query({
-      text: `INSERT INTO books (
+  const result = await database.query({
+    text: `INSERT INTO books (
       title,
       author_name,
       cover_url,
@@ -19,23 +16,21 @@ async function create(bookDataFromApi) {
       VALUES ($1, $2, $3, $4, $5)
       RETURNING *;
         `,
-      values: [
-        bookDataFromApi.title,
-        authorName,
-        bookDataFromApi.cover_url,
-        bookDataFromApi.first_publish_year,
-        bookDataFromApi.key,
-      ],
-    });
+    values: [
+      bookDataFromApi.title,
+      authorName,
+      bookDataFromApi.cover_url,
+      bookDataFromApi.first_publish_year,
+      bookDataFromApi.key,
+    ],
+  });
 
-    return result.rows[0];
-  } catch (error) {
-    console.error("Failed to insert or update book:", error);
-    throw error;
-  } finally {
-    dbClient.end;
-  }
+  return result.rows[0];
 }
+
+// async function findAll()  {
+
+// }
 
 export default {
   create,
